99a103ad2e1769bb7b1e9a91b07ee3ba9d66e2cb
Asmit Tyagi <ashmittyagigms@gmail.com>
2024-01-04 15:40:06 +0530

    learned usecontet , how to excess data in a file using use context

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,13 +1,19 @@
 
 import './App.css'
-import UserContextProvider from './context/UserContext'
+import Login from './componenets/Login'
+
+import Profile from './componenets/Profile'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
+  
 
   return (
-   <UserContextProvider>
-   <h1>This is It</h1>
-   </UserContextProvider>
+    <UserContextProvider>
+      <h1>React with Chai and share is important</h1>
+      <Login/>
+      <Profile />
+    </UserContextProvider>
   )
 }
 
