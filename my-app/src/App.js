e4069c1c1f283cba7f5e02afd76b73c357765586
Asmit Tyagi <ashmittyagigms@gmail.com>
2024-01-07 02:22:03 +0530

    done with Todo application

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,16 @@
+// for (let i = 1; i <=5; i++) {
+//   setTimeout(() => {
+//     console.log(i)
+//   }, i*1000);
+// }
+// console.log("Hey")
 
-
-
-function App() {
-  return (
-    <h1>chai aur react</h1>
-  );
+function x(){
+  for (var i = 1; i <=5; i++) {
+      setTimeout(() => {
+        console.log(i)
+      }, );
+    }
+    console.log("Hey")
 }
-
-export default App;
+x()
